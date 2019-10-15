6c742188005c16c398f6d012acfbdded17913e8d
joao <joao@teste.com>
2019-10-15 10:24:01 -0300

    Busca por nome em funcionamento

--- app.js
+++ app.js
@@ -60,9 +60,34 @@
         app.use(express.static(path.join(__dirname,'public')))
 //Rotas
 //Rota Home
-    app.get('/', (req, res) => {
+    /*app.get('/', (req, res) => {
         res.render("index")
+    })*/
+
+    app.get('/', (req, res) => {
+        Planeta.find().then((planetas) => {
+            //aqui eu passo os dados para o front
+            res.render("index", {planetas: planetas})
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno")
+        })
     })
+
+    app.get("/planets/:nome", (req, res) => {
+        Planeta.findOne({nome: req.params.nome}).then((planeta) => {
+            if(planeta){
+                    res.render("planets/index", {planeta: planeta})
+               
+            }else{
+                req.flash("error_msg", "Este planeta não existe")
+                res.redirect("/")
+            }
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/")
+        })  
+    })
+
     //app.get('/teste', controller.adicionarNoBanco)
 
     app.use('/admin', admin)
